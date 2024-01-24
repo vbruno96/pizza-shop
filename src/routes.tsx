a087d6fbcf4c35976407ae7c0d7b0b7bb7c87a63
Bruno Vinícius <vbruno96@gmail.com>
2024-01-23 22:56:40 -0300

    create orders components

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,11 @@
 import { createBrowserRouter } from 'react-router-dom'
 
-import { AppLayout } from './pages/_layouts/app'
-import { AuthLayout } from './pages/_layouts/auth'
-import { Dashboard } from './pages/app/dashboard'
-import { SignIn } from './pages/auth/sign-in'
-import { SignUp } from './pages/auth/sign-up'
+import { AppLayout } from '@/pages/_layouts/app'
+import { AuthLayout } from '@/pages/_layouts/auth'
+import { Dashboard } from '@/pages/app/dashboard'
+import { Orders } from '@/pages/app/orders/orders'
+import { SignIn } from '@/pages/auth/sign-in'
+import { SignUp } from '@/pages/auth/sign-up'
 
 export const router = createBrowserRouter([
   {
@@ -14,6 +15,10 @@ export const router = createBrowserRouter([
       {
         path: '/',
         element: <Dashboard />,
+      },
+      {
+        path: '/orders',
+        element: <Orders />,
       },
     ],
   },
