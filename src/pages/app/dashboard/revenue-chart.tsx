40cef2b22ef28b733c4a4ec202c809b872d5e1f7
Bruno <vbruno96@gmail.com>
2024-01-24 16:31:16 -0300

    send files for github

---
+++ src/pages/app/dashboard/revenue-chart.tsx
@@ -0,0 +1,84 @@
+import {
+  CartesianGrid,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from 'recharts'
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+
+const data = [
+  {
+    date: '10/12',
+    revenue: 1200,
+  },
+  {
+    date: '11/12',
+    revenue: 800,
+  },
+  {
+    date: '12/12',
+    revenue: 960,
+  },
+  {
+    date: '13/12',
+    revenue: 480,
+  },
+  {
+    date: '14/12',
+    revenue: 2300,
+  },
+  {
+    date: '15/12',
+    revenue: 830,
+  },
+  {
+    date: '16/12',
+    revenue: 640,
+  },
+]
+
+export function RevenueChart() {
+  return (
+    <Card className="col-span-6">
+      <CardHeader className="flex-row items-center justify-between pb-8">
+        <div className="space-y-1">
+          <CardTitle className="text-base font-medium">
+            Receita no período
+          </CardTitle>
+          <CardDescription>Receita diaria no período</CardDescription>
+        </div>
+      </CardHeader>
+      <CardContent>
+        <ResponsiveContainer width="100%" height={240}>
+          <LineChart data={data} style={{ fontSize: 12 }}>
+            <XAxis dataKey="date" />
+
+            <YAxis
+              stroke="#888"
+              axisLine={false}
+              tickLine={false}
+              width={80}
+              tickFormatter={(value: number) =>
+                value.toLocaleString('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL',
+                })
+              }
+            />
+            <Line type="linear" strokeWidth={2} dataKey="revenue" />
+          </LineChart>
+        </ResponsiveContainer>
+      </CardContent>
+    </Card>
+  )
+}
