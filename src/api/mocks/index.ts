fc389e90f4c6baaee4559b538158f222939e2da2
Bruno <vbruno96@gmail.com>
2024-03-21 10:32:55 -0300

    test:create register restaurant mock test

--- src/api/mocks/index.ts
+++ src/api/mocks/index.ts
@@ -1,9 +1,10 @@
 import { setupWorker } from 'msw/browser'
 
+import { registerRestairantMock } from '@/api/mocks/register-restaurant-mock'
 import { signInMock } from '@/api/mocks/sign-in-mock'
 import { env } from '@/env'
 
-export const worker = setupWorker(signInMock)
+export const worker = setupWorker(signInMock, registerRestairantMock)
 
 export async function enableMSW() {
   if (env.MODE !== 'test') return
---
+++ src/api/mocks/register-restaurant-mock.ts
@@ -0,0 +1,16 @@
+import { http, HttpResponse } from 'msw'
+
+import { RegisterRestaurantBody } from '@/api/register-restaurant'
+
+export const registerRestairantMock = http.post<never, RegisterRestaurantBody>(
+  '/restaurants',
+  async ({ request }) => {
+    const { restaurantName } = await request.json()
+
+    if (restaurantName === 'Pizza Shop') {
+      return new HttpResponse(null, { status: 201 })
+    }
+
+    return new HttpResponse(null, { status: 400 })
+  },
+)
