b0ff270753bde1fdd2a2aea023e794006ff7c667
Bruno <vbruno96@gmail.com>
2024-03-25 17:41:41 -0300

    test:create sign up e2e test

---
+++ test/sign-up.e2e-spec.ts
@@ -0,0 +1,56 @@
+import { expect, test } from '@playwright/test'
+
+test('sing up successfuly', async ({ page }) => {
+  await page.goto('/sign-up')
+
+  await page.getByLabel('Nome do estabelecimento').fill('Pizza Shop')
+  await page.getByLabel('Seu nome').fill('John Doe')
+  await page.getByLabel('Seu e-mail').fill('johndoe@example.com')
+  await page.getByLabel('Seu celular').fill('(31) 99999-9999')
+
+  await page.getByRole('button', { name: 'Finalizar cadastro' }).click()
+
+  const toast = page.getByText('Restaurante cadastrado com sucesso!')
+
+  expect(toast).toBeVisible()
+})
+
+test('sing up successfy with redirec to login page and fill email field', async ({
+  page,
+}) => {
+  await page.goto('/sign-up')
+
+  await page.getByLabel('Nome do estabelecimento').fill('Pizza Shop')
+  await page.getByLabel('Seu nome').fill('John Doe')
+  await page.getByLabel('Seu e-mail').fill('johndoe@example.com')
+  await page.getByLabel('Seu celular').fill('(31) 99999-9999')
+
+  await page.getByRole('button', { name: 'Finalizar cadastro' }).click()
+
+  await page.getByRole('button', { name: 'Login' }).click()
+
+  expect(page.url()).toContain('?email=johndoe@example.com')
+})
+
+test('sing up error', async ({ page }) => {
+  await page.goto('/sign-up')
+
+  await page.getByLabel('Nome do estabelecimento').fill('BVM Pizza')
+  await page.getByLabel('Seu nome').fill('John Doe')
+  await page.getByLabel('Seu e-mail').fill('johndoe@example.com')
+  await page.getByLabel('Seu celular').fill('(31) 99999-9999')
+
+  await page.getByRole('button', { name: 'Finalizar cadastro' }).click()
+
+  const toast = page.getByText('Erro ao cadastrar restaurante.')
+
+  expect(toast).toBeVisible()
+})
+
+test('navgate to login page', async ({ page }) => {
+  await page.goto('/sign-up')
+
+  await page.getByRole('link', { name: 'Fazer login' }).click()
+
+  expect(page.url()).toContain('/sign-in')
+})
